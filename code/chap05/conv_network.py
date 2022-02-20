5e04b794433edf1453b81242fb802a12ebbb0172
meticulousdev <meticulousdev@gmail.com>
2022-02-21 00:56:52 +0900

    conv network

--- code/chap05/conv_network.py
+++ code/chap05/conv_network.py
@@ -1,7 +1,9 @@
+# %%
 from keras import layers
 from keras import models
 # from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
 
 import tensorflow as tf
 
@@ -71,3 +73,25 @@ history = model.fit_generator(train_generator,
                               validation_steps=50)
         
 model.save('cats_and_dogs_small_1.h5')
+
+# %%
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
