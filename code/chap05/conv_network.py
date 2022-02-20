19f0aeb85d34d64cab0d48f7fdf4f0c184b1e3c4
meticulousdev <meticulousdev@gmail.com>
2022-02-21 00:50:23 +0900

    conv network

---
+++ code/chap05/conv_network.py
@@ -0,0 +1,73 @@
+from keras import layers
+from keras import models
+# from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+
+import tensorflow as tf
+
+import os
+
+
+# %%
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', 
+                        input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+# %%
+print(model.summary())
+
+# %%
+# model.compile(loss='binary_crossentropy',
+#               optimizer=optimizers.RMSprop(lr=1e-4),
+#               metrics=['acc'])
+
+model.compile(loss='binary_crossentropy',
+              optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-4),
+              metrics=['acc'])
+
+# %%
+original_dataset_dir = './datasets/cats_and_dogs/train'
+
+base_dir = './datasets/cats_and_dogs_small'
+
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    target_size=(150, 150),
+                                                    batch_size=20,
+                                                    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(validation_dir,
+                                                        target_size=(150, 150),
+                                                        batch_size=20,
+                                                        class_mode='binary')
+
+# %%
+for data_batch, labels_batch in train_generator:
+    print(f"data batch shape  : {data_batch.shape}")
+    print(f"labels batch shape: {labels_batch.shape}")
+    break
+
+# %%
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=100,
+                              epochs=30,
+                              validation_data=validation_generator,
+                              validation_steps=50)
+        
+model.save('cats_and_dogs_small_1.h5')
