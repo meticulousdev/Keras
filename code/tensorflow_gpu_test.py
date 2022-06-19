dcbffe98d4126c48bd905155b5b975d06d2287c1
meticulousdev <meticulousdev@gmail.com>
2022-06-19 20:11:53 +0900

    tensorflow gpu test

--- code/tensorflow_gpu_test.py
+++ code/tensorflow_gpu_test.py
@@ -1,2 +1,10 @@
+# %%
 from tensorflow.python.client import device_lib
 device_lib.list_local_devices()
+
+# [name: "/device:CPU:0"
+#  device_type: "CPU"
+#  ...
+#  name: "/device:GPU:0"
+#  device_type: "GPU"
+# ...]
