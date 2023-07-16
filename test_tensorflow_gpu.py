317d8fe36b69477b255d36090661833410fad9cc
meticulousdev <meticulousdev@gmail.com>
2023-07-17 00:13:58 +0900

    gpu test

--- test_tensorflow_gpu.py
+++ test_tensorflow_gpu.py
@@ -1,6 +1,12 @@
 # %%
+import tensorflow as tf
 from tensorflow.python.client import device_lib
-device_lib.list_local_devices()
+
+
+if __name__ == "__main__":
+    print(tf.__version__)
+    print(tf.test.is_built_with_cuda())
+    print(device_lib.list_local_devices())
 
 # [name: "/device:CPU:0"
 #  device_type: "CPU"
