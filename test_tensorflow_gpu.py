7425b5b138668f7ce1c7a7bb5758b9a5878b807f
meticulousdev <meticulousdev@gmail.com>
2023-07-18 22:40:35 +0900

    gpu test

--- test_tensorflow_gpu.py
+++ test_tensorflow_gpu.py
@@ -8,6 +8,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 if __name__ == "__main__":
     print(tf.__version__)
+    print(tf.config.list_physical_devices('GPU'))
     print(tf.test.is_built_with_cuda())
     print(device_lib.list_local_devices())
 
